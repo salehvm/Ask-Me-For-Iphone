3f465153188f397b1592a15a608c55053ef3240a
saleh <salehvm@code.edu.az>
2018-01-22 19:24:46 +0300

    Ask Me Program for only fun mini and good game enjoy

--- Ask Me/ViewController.swift
+++ Ask Me/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var randomBallNumber : Int = 0
+    let ballArry = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    @IBOutlet weak var askmeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       viewDidload()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func askmeBtn(_ sender: UIButton) {
+        viewDidload()
+    }
+    
+    func viewDidload(){
+      
+        randomBallNumber = Int(arc4random_uniform(4))
+        
+        askmeImage.image = UIImage(named: ballArry[randomBallNumber])
+    }
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        viewDidload()
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        viewDidload()
+    }
 
 }
 
